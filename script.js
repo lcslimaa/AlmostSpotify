80edf423a60c768659cd0b72d96609eb8939289a
b4iano <lucas-lima2000@Hotmail.com>
2020-08-25 13:14:51 -0300

    Change button on pause and play

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var audioElement = new Audio("music/RocketPunch.mp3");
 var isPlaying = false;
 var progress = document.getElementById("progress");
 var volumeBar = document.getElementById("volume");
+var ppbutton = document.getElementById("playIcon");
 
 function togglePlay() {
-  isPlaying ? audioElement.pause() : audioElement.play();
+  isPlaying ? pauseMusic() : playMusic();
 }
 
 audioElement.onplaying = function () {
@@ -14,6 +15,18 @@ audioElement.onplaying = function () {
 audioElement.onpause = function () {
   isPlaying = false;
 };
+
+function playMusic() {
+  audioElement.play();
+  ppbutton.classList.remove("fas", "fa-play");
+  ppbutton.classList.add("fas", "fa-pause");
+}
+
+function pauseMusic() {
+  audioElement.pause();
+  ppbutton.classList.remove("fas", "fa-pause");
+  ppbutton.classList.add("fas", "fa-play");
+}
 
 audioElement.addEventListener("timeupdate", function () {
   progress.value = audioElement.currentTime / audioElement.duration;
